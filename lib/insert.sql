60219db50639878bf5d111bd535dae26116d76f1
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-07 05:29:17 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Mr.Chocolate", 15, "male", "brown", "unpredictable" ,1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Rowdy", 3, "male", "dark brown", "unpredictable" ,1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Tabitha", 6, "female", "black", "aggresive" ,0);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Sergeant Brwon", 7, "male", "brown", "angry" ,1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Melissa", 13, "female", "black", "aggresive" ,1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Grinch", 2, "male", "white", "calm" ,1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Wendy", 6, "female", "dark brown", "unpredictable" ,0);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES (Null, 12, "male", "brown", "aggresive" ,0);
